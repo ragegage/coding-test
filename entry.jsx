71dce105a52ae6471bd0dbdc06a77965495a7a40
Rage Gage <ragegage@Rages-MacBook-Air.local>
2016-08-24 10:12:56 -0700

    adds basic app structure

---
+++ entry.jsx
@@ -0,0 +1,7 @@
+import { render } from 'react-dom'
+import React from 'react'
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("content")
+  render(<div>test</div>, root)
+})
